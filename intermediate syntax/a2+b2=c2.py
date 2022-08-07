b175cea14955ceb02d3f73c59e20e4506dd41ba0
Nicola leone <20nleone@conyers.org.uk>
2022-08-07 21:24:35 +0200

    ok

--- intermediate syntax/a2+b2=c2.py
+++ intermediate syntax/a2+b2=c2.py
@@ -1,1 +1,25 @@
-enter int(input(""))
+first_side = float(input("enter the value of the first side angle : "))
+second_side = float(input("enter the value of the second side in your triangle : "))
+question = input("what side do you want to know the value of : ( a , b , c ) ")
+hypotenuse = (first_side**2 + second_side**2)**0.5
+first_base = hypotenuse - second_side or second_side - hypotenuse
+second_base = hypotenuse - first_side or first_side - hypotenuse
+if question == "a":
+    print("the value of a is : ", first_base)
+elif question == "b":
+    print("the value of b is : ", second_base)
+elif question == "c":
+    print("the value of c is : ", hypotenuse)
+
+
+
+
+
+
+
+
+
+
+
+
+
