2f26d173c8fc5d2a8302ae947d8d1325567f2d30
Nicola leone <20nleone@conyers.org.uk>
2022-08-08 10:08:21 +0200

    int

---
+++ intermediate syntax/int.py
@@ -0,0 +1,9 @@
+integer = int(input("enter the first digit: "))
+integer2 = int(input("enter the second digit: "))
+sum = integer + integer2 
+if sum => 80:
+    print("overflow")
+else:
+    print(sum)   
+
+
